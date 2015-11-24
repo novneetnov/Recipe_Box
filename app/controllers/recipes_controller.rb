a822f4ae70f3c79a02c4c73c440b9ff2962e15e2
novneetnov <novneet346iith@gmail.com>
2015-11-24 17:58:10 +0530

    Add Recipe Model and Migrate Database and Add Resources to Routes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,37 @@
 class RecipesController < ApplicationController
+	before_action :find_recipe, only: [:show, :edit, :update, :destroy]
 
 	def index
+		@recipes = Recipe.all
+	end
+
+	def new
+		@recipe = Recipe.new
+	end
+
+	def create
+		@recipe = Recipe.new(recipe_params)
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+	def find_recipe
+		@recipe = Recipe.find(params[:id])
+	end
+
+	def recipe_params
+		params.require(:recipe).permit(:title, :description)
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
 	root "recipes#index"
+	resources :recipes
 
 end
